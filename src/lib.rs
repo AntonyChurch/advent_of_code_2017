1aa1ac98262ac41919aeb7cdca8ad93654ac1de6
Antony Church <achurch44.ac@gmail.com>
2017-12-05 21:26:53 +0000

    Finished Day 1

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ pub fn one_a(input: String) -> u32 {
     let mut i = 0;
 
     loop {
-        if i > input_ints.len() {
+        if i >= input_ints.len() {
             break;
         }
 
@@ -24,13 +24,42 @@ pub fn one_a(input: String) -> u32 {
         i = i + 1;        
     }
 
-    let mut sum: u32 = 0;
+    return sum_int_vec(to_sum);
+}
 
-    for x in to_sum.iter() {
-        sum += x;
+pub fn one_b(input: String) -> u32 {
+    let input_ints = get_int_list_from_string(input);
+    let mut to_sum: Vec<u32> = Vec::new();
+
+    let mut split_int1 = input_ints.clone();
+    let split_int2 = split_int1.split_off(input_ints.len() / 2);
+
+    let mut combined_ints: Vec<u32> = Vec::new();
+    for int in split_int2.iter() {
+        let x: u32 = *int;
+        combined_ints.push(x);
     }
 
-    return sum;
+    for int in split_int1.iter() {
+        let x: u32 = *int;
+        combined_ints.push(x);
+    }
+
+    let mut i = 0;
+
+    loop {
+        if i >= input_ints.len() {
+            break;
+        }
+
+        if input_ints[i] == combined_ints[i] {
+            to_sum.push(input_ints[i]);
+        }
+
+        i = i + 1;
+    }
+
+    return sum_int_vec(to_sum);
 }
 
 fn get_int_list_from_string(input: String) -> Vec<u32> {
@@ -45,45 +74,12 @@ fn get_int_list_from_string(input: String) -> Vec<u32> {
     return list;
 }
 
-#[test]
-fn example_one(){        
-    assert_eq!(3, one_a(String::from("1122")));
+fn sum_int_vec(input: Vec<u32>) -> u32 {
+    let mut sum: u32 = 0;
+
+    for x in input.iter() {
+        sum += x;
+    }
+
+    return sum; 
 }
-
-#[test]
-fn example_two(){
-    assert_eq!(4, one_a(String::from("1111")));
-}
-
-#[test]
-fn example_three(){
-    assert_eq!(0, one_a(String::from("1234")));
-}
-
-#[test]
-fn example_four(){
-    assert_eq!(9, one_a(String::from("91212129")));
-}
-
-// #[cfg(test)]
-// mod day_one_tests {
-//     #[test]
-//     fn example_one(){        
-//         assert_eq!(3, one_a(String::from("1122")));
-//     }
-
-//     #[test]
-//     fn example_two(){
-//         assert_eq!(4, one_a(String::from("1111")));
-//     }
-
-//     #[test]
-//     fn example_three(){
-//         assert_eq!(0, one_a(String::from("1234")));
-//     }
-
-//     #[test]
-//     fn example_four(){
-//         assert_eq!(9, one_a(String::from("91212129")));
-//     }
-// }
--- tests/day_one_tests.rs
+++ tests/day_one_tests.rs
@@ -1,24 +1,46 @@
-// extern crate adventofcode;
+extern crate adventofcode;
 
-// #[cfg(test)]
-// mod day_one_tests {
-//     #[test]
-//     fn example_one(){        
-//         assert_eq!(3, one_a(String::from("1122")));
-//     }
+#[test]
+fn example_a_one(){        
+    assert_eq!(3, adventofcode::one_a(String::from("1122")));
+}
 
-//     #[test]
-//     fn example_two(){
-//         assert_eq!(4, one_a(String::from("1111")));
-//     }
+#[test]
+fn example_a_two(){
+    assert_eq!(4, adventofcode::one_a(String::from("1111")));
+}
 
-//     #[test]
-//     fn example_three(){
-//         assert_eq!(0, one_a(String::from("1234")));
-//     }
+#[test]
+fn example_a_three(){
+    assert_eq!(0, adventofcode::one_a(String::from("1234")));
+}
 
-//     #[test]
-//     fn example_four(){
-//         assert_eq!(9, one_a(String::from("91212129")));
-//     }
-// }
+#[test]
+fn example_a_four(){
+    assert_eq!(9, adventofcode::one_a(String::from("91212129")));
+}
+
+#[test]
+fn exmaple_b_one(){
+    assert_eq!(6, adventofcode::one_b(String::from("1212")));
+}
+
+#[test]
+fn exmaple_b_two(){
+    assert_eq!(0, adventofcode::one_b(String::from("1221")));
+}
+
+#[test]
+fn exmaple_b_three(){
+    assert_eq!(4, adventofcode::one_b(String::from("123425")));
+}
+
+#[test]
+fn exmaple_b_four(){
+    assert_eq!(12, adventofcode::one_b(String::from("123123")));
+}
+
+#[test]
+fn exmaple_b_five(){
+    assert_eq!(4, adventofcode::one_b(String::from("12131415")));
+}
